015c9d99d27547eae20629312f1dc923d17224b1
Saurabh Pawar <rebasedev01@gmail.com>
2021-10-19 13:01:13 +0530

    Data Added.

--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,102 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
 const yamljs_1 = __importDefault(require("yamljs"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const app = (0, express_1.default)();
 const swaggerDocument = yamljs_1.default.load('./swaggerdoc.yaml');
+const data = [{
+        artists_data: [
+            {
+                artist_name: 'Arjit',
+                artist_id: '1234',
+                match_percentage: 48,
+                match_attributes: {
+                    venues: [
+                        {
+                            name: 'Sunburn',
+                            match_percentage: 20,
+                        },
+                    ],
+                    gender: 'Male',
+                    genre: 'Bollywood',
+                    associated_brands: [
+                        {
+                            name: 'Coke Studio',
+                            contact: 'cokestudio.contact@coke.com',
+                            website: 'coke.com',
+                        },
+                    ],
+                },
+                summary: 'Arjit is well known bollywod singer with lot of hits',
+            },
+        ],
+    }, {
+        artists_data: [
+            {
+                artist_name: 'Atif',
+                artist_id: '1254',
+                match_percentage: 30,
+                match_attributes: {
+                    venues: [
+                        {
+                            name: 'Radio Mirchi',
+                            match_percentage: 60,
+                        },
+                    ],
+                    gender: 'Male',
+                    genre: 'Bollywood',
+                    associated_brands: [
+                        {
+                            name: 'MTV Radio',
+                            contact: 'MTVradio.contact@mtv.com',
+                            website: 'mtv.com',
+                        },
+                    ],
+                },
+                summary: 'Atif is well known bollywod singer with lot of hits',
+            },
+        ],
+    }, {
+        artists_data: [
+            {
+                artist_name: 'Prateek Kullhad',
+                artist_id: '1546',
+                match_percentage: 70,
+                match_attributes: {
+                    venues: [
+                        {
+                            name: 'Concert',
+                            match_percentage: 60,
+                        },
+                    ],
+                    gender: 'Male',
+                    genre: 'Bollywood',
+                    associated_brands: [
+                        {
+                            name: 'VH1 India',
+                            contact: 'vh1.contact@vh1.com',
+                            website: 'vh1.com',
+                        },
+                    ],
+                },
+                summary: 'Prateek is well known bollywod singer with lot of hits',
+            },
+        ],
+    }];
 app.use(body_parser_1.default.json());
 app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
-app.get("/", (req, res) => {
-    res.send("HELLO!");
+app.get('/', (req, res) => {
+    res.send('HELLO!');
 });
-app.post("/recommender/api/getMatchData/", (req, res) => {
-    const data = JSON.stringify(req.body);
+app.post('/recommender/api/getMatchData/', (req, res) => {
+    const body = JSON.stringify(req.body);
     res.send(data);
 });
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log('Server running on port 3000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,102 @@
-import express, { json } from "express";
-const app = express();
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+import express, { json } from 'express';
 import YAML from 'yamljs';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
+
+const app = express();
+
 const swaggerDocument = YAML.load('./swaggerdoc.yaml');
 
-
+const data = [{
+  artists_data: [
+    {
+      artist_name: 'Arjit',
+      artist_id: '1234',
+      match_percentage: 48,
+      match_attributes: {
+        venues: [
+          {
+            name: 'Sunburn',
+            match_percentage: 20,
+          },
+        ],
+        gender: 'Male',
+        genre: 'Bollywood',
+        associated_brands: [
+          {
+            name: 'Coke Studio',
+            contact: 'cokestudio.contact@coke.com',
+            website: 'coke.com',
+          },
+        ],
+      },
+      summary: 'Arjit is well known bollywod singer with lot of hits',
+    },
+  ],
+}, {
+  artists_data: [
+    {
+      artist_name: 'Atif',
+      artist_id: '1254',
+      match_percentage: 30,
+      match_attributes: {
+        venues: [
+          {
+            name: 'Radio Mirchi',
+            match_percentage: 60,
+          },
+        ],
+        gender: 'Male',
+        genre: 'Bollywood',
+        associated_brands: [
+          {
+            name: 'MTV Radio',
+            contact: 'MTVradio.contact@mtv.com',
+            website: 'mtv.com',
+          },
+        ],
+      },
+      summary: 'Atif is well known bollywod singer with lot of hits',
+    },
+  ],
+}, {
+  artists_data: [
+    {
+      artist_name: 'Prateek Kullhad',
+      artist_id: '1546',
+      match_percentage: 70,
+      match_attributes: {
+        venues: [
+          {
+            name: 'Concert',
+            match_percentage: 60,
+          },
+        ],
+        gender: 'Male',
+        genre: 'Bollywood',
+        associated_brands: [
+          {
+            name: 'VH1 India',
+            contact: 'vh1.contact@vh1.com',
+            website: 'vh1.com',
+          },
+        ],
+      },
+      summary: 'Prateek is well known bollywod singer with lot of hits',
+    },
+  ],
+}];
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.get("/", (req, res)=>{
-    res.send("HELLO!")
-})
-app.post("/recommender/api/getMatchData/", (req, res)=>{
-const data = JSON.stringify(req.body)
-res.send(data)
-})
+app.get('/', (req, res) => {
+  res.send('HELLO!');
+});
+app.post('/recommender/api/getMatchData/', (req, res) => {
+  const body = JSON.stringify(req.body);
+  res.send(data);
+});
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+  console.log('Server running on port 3000');
 });
