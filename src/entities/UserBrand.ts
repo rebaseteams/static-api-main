12b7663881bdc69e245fe66f6fa9d2587a8f6d56
Saurabh Pawar <rebasedev01@gmail.com>
2021-10-27 16:44:38 +0530

    linting fixed.

--- src/entities/UserBrand.ts
+++
@@ -1,9 +0,0 @@
-export class UserBrand {
-    private brand_name;
-    private brand_id;
-    
-    constructor(brand_name : string, brand_id : string) {
-        this.brand_name = brand_name;
-        this.brand_id = brand_id;
-    }
-}
