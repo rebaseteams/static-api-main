e982ceb4357aff27ab2b8227ff5d0a1b302216ea
prasanarebase <prasana.shinde.rebase@gmail.com>
2021-10-27 13:15:58 +0530

    Added userbrand to entities

---
+++ src/entities/UserBrand.ts
@@ -0,0 +1,9 @@
+export class UserBrand {
+    private brand_name;
+    private brand_id;
+    
+    constructor(brand_name : string, brand_id : string) {
+        this.brand_name = brand_name;
+        this.brand_id = brand_id;
+    }
+}
