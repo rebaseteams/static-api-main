60174b8c092aac098a0befccd5a1bab3f106b446
prasanarebase <prasana.shinde.rebase@gmail.com>
2021-10-28 10:38:04 +0530

    disabled container

--- src/container.ts
+++ src/container.ts
@@ -1,21 +1,21 @@
-import { createContainer, asClass } from 'awilix';
+// import { createContainer, asClass } from 'awilix';
 
-import Server from './interfaces/http/server';
-import Application from './app/app';
-import CreateUserBrand from './app/userbrand/create';
-import RemoveUserBrand from './app/userbrand/remove';
-import GetUserBrand from './app/userbrand/get';
-import UpdateUserBrand from './app/userbrand/update';
-import SearchUserBrand from './app/userbrand/search';
+// import Server from './interfaces/http/server';
+// import Application from './app/app';
+// import CreateUserBrand from './app/userbrand/create';
+// import RemoveUserBrand from './app/userbrand/remove';
+// import GetUserBrand from './app/userbrand/get';
+// import UpdateUserBrand from './app/userbrand/update';
+// import SearchUserBrand from './app/userbrand/search';
 
-const container = createContainer();
+// const container = createContainer();
 
-container.register({
-  server: asClass(Server).singleton(),
-  app: asClass(Application).singleton(),
-  createUserBrand: asClass(CreateUserBrand).singleton(),
-  removeUserBrand: asClass(RemoveUserBrand).singleton(),
-  updateUserBrand: asClass(UpdateUserBrand).singleton(),
-  GetUserBrand: asClass(GetUserBrand).singleton(),
-  searchUserBrand: asClass(SearchUserBrand).singleton(),
-});
+// container.register({
+//   server: asClass(Server).singleton(),
+//   app: asClass(Application).singleton(),
+//   createUserBrand: asClass(CreateUserBrand).singleton(),
+//   removeUserBrand: asClass(RemoveUserBrand).singleton(),
+//   updateUserBrand: asClass(UpdateUserBrand).singleton(),
+//   GetUserBrand: asClass(GetUserBrand).singleton(),
+//   searchUserBrand: asClass(SearchUserBrand).singleton(),
+// });
