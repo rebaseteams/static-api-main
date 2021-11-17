7d1122f70562eb653882957234773547e9936919
Saurabh Pawar <rebasedev01@gmail.com>
2021-11-17 17:58:38 +0530

    Swagger error solved.

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -11,7 +11,7 @@ const validateToken = (token: string | string[]): Boolean => {
 
 // eslint-disable-next-line consistent-return
 const validateUser = ():((req: Request, res: Response, next: NextFunction) => void) => async (req: Request, res: Response, next: NextFunction) => {
-  const notSecurePath = ['/api-docs'];
+  const notSecurePath = ['/auth/login'];
 
   if (notSecurePath.includes(req.path)) {
     return next();
