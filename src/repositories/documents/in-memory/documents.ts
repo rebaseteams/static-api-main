ec0d308796d8dad525b2da7368f691117f5ec051
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-24 15:40:40 +0530

    Updated template

--- src/repositories/documents/in-memory/documents.ts
+++ src/repositories/documents/in-memory/documents.ts
@@ -8,7 +8,12 @@ export default class InMemoryDocumentsRepo implements DocumentsInterface {
   // eslint-disable-next-line class-methods-use-this
   sendHtmlTemplates(options: DocumentInput): string {
     // eslint-disable-next-line no-console
-    const response = `<h4>Hey ${options.artistName}</h4><h6>We want to colaborate with you because ${options.reason}</h4>`;
+    const response = `<h2><span style="color: #fbeeb8;">Hey ${options.artistName},</span></h2>
+    <h3>We <span style="color: #2dc26b;">liked</span> your profile on <span style="color: #f1c40f;">Cuttime.</span></h3>
+    <p>We are planning to have a Concert at <strong><span style="color: #3598db;">London, Imperial Collge,</span></strong></p>
+    <p>and want you to collaborate with us.</p>
+    <p>we liked you because ${options.reason}</p>
+    <p><strong>Regards Imperial College.</strong></p>`;
     return response;
   }
 }
