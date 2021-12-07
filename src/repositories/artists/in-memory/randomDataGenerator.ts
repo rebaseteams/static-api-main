475c602cc402f8b0ea828689b91f22544d0b0dcb
prasana shinde <prasana.shinde.rebase@gmail.com>
2021-12-07 17:47:33 +0530

    added randomly generated data

---
+++ src/repositories/artists/in-memory/randomDataGenerator.ts
@@ -0,0 +1,123 @@
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import * as fs from 'fs';
+import { createConnection } from 'typeorm';
+import Artist from '../../../models/entities/Artist';
+
+const randomDataGenerator = async () => {
+  const artistList = [];
+  createConnection(
+    {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'concert_curator',
+      synchronize: false,
+      entities: [Artist],
+    },
+  ).then(async (connection) => {
+    for (let i = 1; i < 50; i += 1) {
+      const profile = await (await axios.get('https://randomuser.me/api/')).data.results[0];
+      const gen = Math.floor(Math.random() * 99);
+      const artistObj = new Artist(
+        profile.login.uuid,
+        `${profile.name.first} ${profile.name.last}`,
+        profile.gender,
+        [uuidv4(), uuidv4(), uuidv4()],
+        [uuidv4(), uuidv4(), uuidv4()],
+        profile.location.country,
+        profile.picture.thumbnail,
+        profile.picture.large,
+        `${profile.name.first} ${profile.name.last} lives in ${profile.location.country}. ${profile.name.first} ${profile.name.last} is ${profile.gender}`,
+        uuidv4(),
+        profile.phone,
+        `${profile.location.city},${profile.location.state},${profile.location.country},${profile.location.postcode}`,
+        Math.floor(Math.random() * 99),
+        JSON.stringify([
+          {
+            demographicName: 'gender',
+            fields: [
+              {
+                name: 'male',
+                value: gen,
+              },
+              {
+                name: 'female',
+                value: 100 - gen,
+              },
+            ],
+          },
+          {
+            demographicName: 'genre',
+            fields: [
+              {
+                name: 'rock',
+                value: Math.floor(Math.random() * 30),
+              },
+              {
+                name: 'hip-hop',
+                value: Math.floor(Math.random() * 30),
+              },
+              {
+                name: 'jazz',
+                value: Math.floor(Math.random() * 30),
+              },
+            ],
+          },
+          {
+            demographicName: 'age group',
+            fields: [
+              {
+                name: '18 - 25',
+                value: Math.floor(Math.random() * 25),
+              },
+              {
+                name: '26 - 35',
+                value: Math.floor(Math.random() * 25),
+              },
+              {
+                name: '36 - 60',
+                value: Math.floor(Math.random() * 25),
+              },
+              {
+                name: '60 +',
+                value: Math.floor(Math.random() * 25),
+              },
+            ],
+          },
+        ]),
+        JSON.stringify([
+          {
+            handleName: 'Youtube',
+            url: 'https://www.youtube.com/c/PehchanMusic',
+            logo: 'https://w7.pngwing.com/pngs/936/468/png-transparent-youtube-logo-youtube-logo-computer-icons-subscribe-angle-rectangle-airplane.png',
+            followers: Math.floor(Math.random() * 1000000),
+          },
+          {
+            handleName: 'Facebook',
+            url: 'https://www.facebook.com/pehchanmusic/',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/1200px-2021_Facebook_icon.svg.png',
+            followers: Math.floor(Math.random() * 1000000),
+          },
+          {
+            handleName: 'Spotify',
+            url: 'https://open.spotify.com/playlist/5cPUQKih2Nan0u9nbhXtCu',
+            logo: 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1475248336/qmsen1jtcy8xgsghzc8z.jpg',
+            followers: Math.floor(Math.random() * 1000000),
+          },
+        ]),
+      );
+      connection.manager.save(artistObj);
+      artistObj.media_handles = JSON.parse(artistObj.media_handles);
+      artistObj.audience = JSON.parse(artistObj.audience);
+      artistList.push(artistObj);
+    }
+    fs.writeFileSync(`${__dirname}/data/artists.json`, JSON.stringify(artistList));
+  });
+};
+
+randomDataGenerator();
+
+export default randomDataGenerator;
--- src/routes/artists/artistRoutes.ts
+++ src/routes/artists/artistRoutes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import ArtistService from '../../services/artist';
 import DocumentsService from '../../services/documents';
 import TemplatesService from '../../services/templates';
+import snakeToCamel from '../../utils/snakeToCamel';
 import RecommendationsRoute from './recommendations/recommendationsRoutes';
 
 export default class ArtistRoute {
@@ -12,7 +13,7 @@ export default class ArtistRoute {
     this.router.use('/recommendations', new RecommendationsRoute(artistService, documentsService, templatesService).router);
     this.router.get('/:id', async (req, res, next) => {
       try {
-        if (req.params.id) res.send(await artistService.getArtist(req.params.id));
+        if (req.params.id) res.send(snakeToCamel(await artistService.getArtist(req.params.id)));
       } catch (error) {
         next(error);
       }
