ba6b51f4e7816b430a1cd4d4633bf51af65b46e7
prasana shinde <prasana.shinde.rebase@gmail.com>
2021-12-13 11:29:47 +0530

    Created  brands postgres repo(mock) and Created services for brand

---
+++ src/repositories/brands/postgres/brand.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+import { v4 as uuidv4 } from 'uuid';
+import Brand from '../../../models/entities/Brand';
+import { BrandsInterface } from '../../../models/interfaces/brand';
+
+export default class BrandRepo implements BrandsInterface {
+  async createBrand(name : string, logo : string, website : string, contact : string) : Promise<{brand : Brand}> {
+    // TODO : Create brand in Postgres
+    const brand = new Brand(
+      uuidv4(),
+      name,
+      logo,
+      website,
+      contact,
+    );
+    return { brand };
+  }
+
+  async getBrand(id : string) : Promise<Brand> {
+    // TODO : Get brand from postgres
+    const brand = new Brand(
+      id,
+      'name',
+      'logo',
+      'website',
+      'contact',
+    );
+    return brand;
+  }
+
+  async deleteBrand(id : string) : Promise<{success : boolean}> {
+    // TODO : Delete brand from postgres
+    console.log(id);
+    return { success: true };
+  }
+
+  async editBrand(id : string) : Promise<{success : boolean}> {
+    // TODO : Edit brand from postgres
+    console.log(id);
+    return { success: true };
+  }
+
+  async getBrands(skip : number, limit : number) : Promise<Brand[]> {
+    // TODO : Get brands with given pagination conditions from postgres
+    console.log(skip, limit);
+    const brands = [new Brand(
+      uuidv4(),
+      'name',
+      'logo',
+      'website',
+      'contact',
+    )];
+    return brands;
+  }
+}
---
+++ src/services/brand.ts
@@ -0,0 +1,32 @@
+import Brand from '../models/entities/Brand';
+import { BrandsInterface } from '../models/interfaces/brand';
+
+export default class BrandsService implements BrandsInterface {
+  private brandRepo: BrandsInterface;
+
+  constructor(
+    brandRepo: BrandsInterface,
+  ) {
+    this.brandRepo = brandRepo;
+  }
+
+  async createBrand(name : string, logo : string, website : string, contact : string) : Promise<{brand : Brand}> {
+    return this.brandRepo.createBrand(name, logo, website, contact);
+  }
+
+  async editBrand(id : string, name : string, logo : string, website : string, contact : string) : Promise<{success : boolean}> {
+    return this.brandRepo.editBrand(id, name, logo, website, contact);
+  }
+
+  async getBrand(id : string) : Promise<Brand> {
+    return this.brandRepo.getBrand(id);
+  }
+
+  async getBrands(skip : number, limit : number) : Promise<Brand[]> {
+    return this.brandRepo.getBrands(skip, limit);
+  }
+
+  async deleteBrand(id : string) : Promise<{success : boolean}> {
+    return this.brandRepo.deleteBrand(id);
+  }
+}
