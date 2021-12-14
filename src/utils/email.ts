2f5c3255c4a572e7dd1d8401d7980d073a43ebc1
ppproduction <prasana.shinde.sd@gmail.com>
2021-12-14 16:11:14 +0530

    setup sendgrid account and create utility to be used in repositories

---
+++ src/utils/email.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import { AttachmentJSON } from '@sendgrid/helpers/classes/attachment';
+import sgMail from '@sendgrid/mail';
+
+const key = '';
+const from = 'prasana.shinde.rebase@gmail.com';
+sgMail.setApiKey(key);
+
+const sendEmail = async (
+  {
+    to, subject, text, html, attachments,
+  } :
+  {
+    to : string,
+    subject : string,
+    text? : string,
+    html? : string,
+    attachments? : AttachmentJSON[],
+  } & ({text : string} | {html : string}),
+) : Promise<{success : boolean}> => {
+  const msg : sgMail.MailDataRequired = {
+    to,
+    from,
+    subject,
+    text,
+    html,
+    attachments,
+  };
+  const resp = await sgMail.send(msg);
+  console.log(resp);
+  return { success: true };
+};
+
+export default sendEmail;
