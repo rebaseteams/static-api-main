2695ffaa4e553ff69d1119fd1cf252818fa9a15f
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-12-23 09:38:38 +0530

    Temprarily solved the create document error

--- src/repositories/documents/in-memory/documents.ts
+++ src/repositories/documents/in-memory/documents.ts
@@ -2,7 +2,7 @@ import * as handlebars from 'handlebars';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import { Express } from 'express';
-import axios from 'axios';
+// import axios from 'axios';
 import { AttachmentJSON } from '@sendgrid/helpers/classes/attachment';
 import { DocumentsInterface } from '../../../models/interfaces/documents';
 import { Template } from '../../../models/types/template';
@@ -14,19 +14,21 @@ export default class InMemoryDocumentsRepo implements DocumentsInterface {
   // eslint-disable-next-line no-unused-vars
   async createDocument(data : any, required : any, template : Template, recommendationId : string, docName : string, userId : string) : Promise<{ document : Document }> {
     let requiredResources = {};
-    if (template.resources) {
-      // eslint-disable-next-line no-restricted-syntax
-      for await (const resource of template.resources) {
-        const compiledUrl = handlebars.compile(resource.url);
-        requiredResources[resource.name] = (await axios.get(compiledUrl(required), { headers: { userid: '1238989' } })).data;
-      }
-    }
+    // Tempararyly disabling error prone code
+    // if (template.resources) {
+    //   // eslint-disable-next-line no-restricted-syntax
+    //   for await (const resource of template.resources) {
+    //     const compiledUrl = handlebars.compile(resource.url);
+    //     requiredResources[resource.name] = (await axios.get(compiledUrl(required), { headers: { userid: '1238989' } })).data;
+    //   }
+    // }
     requiredResources = {
       ...requiredResources,
       ...data,
     };
     const { html } = template;
-    const compiledHtml = handlebars.compile(fs.readFileSync(`src/repositories/templates/in-memory/data/html/${html}`).toString().replace(/\n/g, '').replace(/(^"|"$)/g, ''));
+    const templateHtml = `./data/html/${html}`;
+    const compiledHtml = handlebars.compile(fs.readFileSync(templateHtml).toString().replace(/\n/g, '').replace(/(^"|"$)/g, ''));
     const document : Document = {
       id: uuidv4(),
       template_id: template.templateId,
@@ -37,8 +39,10 @@ export default class InMemoryDocumentsRepo implements DocumentsInterface {
     };
     fileCheck(`${__dirname}/data`, false);
     fileCheck(`${__dirname}/data/html`, false);
-    fs.writeFileSync(`${__dirname}/data/html/${document.id}.html`, JSON.stringify(compiledHtml(requiredResources)));
-    fs.writeFileSync(`${__dirname}/data/${document.id}.json`, JSON.stringify(document));
+    const htmlFile = `${__dirname}/data/html/${document.id}.html`;
+    fs.writeFileSync(htmlFile, JSON.stringify(compiledHtml(requiredResources)));
+    const documentFile = `${__dirname}/data/${document.id}.json`;
+    fs.writeFileSync(documentFile, JSON.stringify(document));
     return { document };
   }
 
@@ -46,9 +50,11 @@ export default class InMemoryDocumentsRepo implements DocumentsInterface {
     fileCheck(`${__dirname}/data`, false);
     const allDocuments : Document[] = [];
     fs.readdirSync(`${__dirname}/data`).forEach((file) => {
-      const toread = fs.readFileSync(`${__dirname}/data/${file}`).toString();
-      const dataJson = JSON.parse(toread) as Document;
-      allDocuments.push(dataJson);
+      if (file !== 'html') {
+        const toread = fs.readFileSync(`${__dirname}/data/${file}`).toString();
+        const dataJson = JSON.parse(toread) as Document;
+        allDocuments.push(dataJson);
+      }
     });
     return allDocuments;
   }
