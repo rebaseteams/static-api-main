07ca109cf9f98782a68b60632c746b2261358f66
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-01-05 10:36:18 +0530

    added fields mode and contract in pg repo

--- src/models/entities/Document.ts
+++ src/models/entities/Document.ts
@@ -1,14 +1,24 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { DocumentContractData, DocumentMode } from '../types/documentContract';
 
 @Entity()
 export default class Document {
-  constructor(id: string, template_id: string, name: string, createdBy: string, createdOn: Date, html: string) {
+  constructor(id: string,
+    template_id: string,
+    name: string,
+    createdBy: string,
+    createdOn: Date,
+    mode: DocumentMode,
+    html: string,
+    contract: DocumentContractData) {
     this.id = id;
     this.template_id = template_id;
     this.name = name;
     this.created_by = createdBy;
     this.created_on = createdOn;
+    this.mode = mode;
     this.html = html;
+    this.contract = contract;
   }
 
   @PrimaryColumn()
@@ -26,6 +36,22 @@ export default class Document {
   @Column({ type: 'timestamp' })
   created_on: Date;
 
+  @Column({ default: 'edit', nullable: true })
+  mode: DocumentMode;
+
   @Column({ type: 'text' })
   html: string;
+
+  @Column({
+    type: 'jsonb',
+    nullable: true,
+    default: {
+      envelopeId: '',
+      url: '',
+      dateCreated: '',
+      signDate: '',
+      status: '',
+    },
+  })
+  contract: DocumentContractData;
 }
