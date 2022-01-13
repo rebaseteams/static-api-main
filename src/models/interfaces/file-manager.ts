e10557d7deb903456e6942809db2089fdd56f093
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-01-13 16:19:30 +0530

    Using Filemanager aws downloadFile to download the html when creating the document

--- src/models/interfaces/file-manager.ts
+++ src/models/interfaces/file-manager.ts
@@ -3,6 +3,6 @@
 import { FileType } from '../types/file-manager';
 
 export interface FileManagerInterface {
-  uploadFile: (id: string, folder: string, type: FileType, data: Buffer) => Promise<{ success: boolean, message: string }>;
-  downloadFile: (id: string, folder: string, type: FileType) => Promise<{ success: boolean, data: Buffer }>;
+  uploadFile: (id: string, data: Buffer) => Promise<{ success: boolean, message: string }>;
+  downloadFile: (id: string) => Promise<{ success: boolean, data: Buffer | string }>;
 }
--- src/repositories/file-manager/s3-file-manager/awsS3.ts
+++ src/repositories/file-manager/s3-file-manager/awsS3.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 import AWS from 'aws-sdk';
 import { FileManagerInterface } from '../../../models/interfaces/file-manager';
-import { AWS_S3_CONFIG, FileType } from '../../../models/types/file-manager';
+import { AWS_S3_CONFIG } from '../../../models/types/file-manager';
 
 export class FileManagerAWSS3Repo implements FileManagerInterface {
   private config: AWS_S3_CONFIG;
 
-  uploadFile = async (id: string, folder: string, type: FileType, data: Buffer): Promise<{ success: boolean, message: string }> => {
+  uploadFile = async (id: string, data: Buffer): Promise<{ success: boolean, message: string }> => {
     this.config = {
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
@@ -14,15 +14,11 @@ export class FileManagerAWSS3Repo implements FileManagerInterface {
     };
     AWS.config.update(this.config);
 
-    const key = folder.length === 0 ? `${id}.${type}` : `${folder}/${id}.${type}`;
-
     const s3 = new AWS.S3(this.config);
     const params = {
       Bucket: process.env.AWS_S3_BUCKET,
-      Key: key,
+      Key: id,
       Body: JSON.stringify(data),
-      folder,
-      type,
     };
 
     return new Promise((resolve, reject) => {
@@ -37,7 +33,7 @@ export class FileManagerAWSS3Repo implements FileManagerInterface {
   };
 
   // eslint-disable-next-line no-unused-vars
-  downloadFile = async (id: string, folder: string, type: FileType):
+  downloadFile = async (id: string):
   Promise<{ success: boolean, data: Buffer | string }> => new Promise((resolve) => {
     try {
       this.config = {
@@ -47,10 +43,9 @@ export class FileManagerAWSS3Repo implements FileManagerInterface {
       };
       AWS.config.update(this.config);
       const s3 = new AWS.S3(this.config);
-      const key = folder.length === 0 ? `${id}.${type}` : `${folder}/${id}.${type}`;
       const options = {
         Bucket: process.env.AWS_S3_BUCKET,
-        Key: key,
+        Key: id,
       };
 
       const fileStream = s3.getObject(options).createReadStream();
@@ -62,7 +57,7 @@ export class FileManagerAWSS3Repo implements FileManagerInterface {
       });
       fileStream.on('error', (err) => resolve({ success: false, data: err.message }));
     } catch (err) {
-      resolve({ success: false, data: null });
+      resolve({ success: false, data: err });
     }
   })
 }
