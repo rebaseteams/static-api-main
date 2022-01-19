3908bdef4b6c3c1be06f6d2ab688fbd14adaeec0
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-19 17:35:20 +0530

    checks token at the start

--- src/utils/tokenValidator.ts
+++ src/utils/tokenValidator.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 
-const tokenValidator = async (token : string) => {
+const tokenValidator = (token : string) => {
   if (token && jwt.decode(token)) {
-    const payload : any = await jwt.decode(token);
+    const payload : any = jwt.decode(token);
     const { exp } = payload;
     const now = new Date();
     return now.getTime() > exp * 1000;
