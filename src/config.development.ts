a69a99af4e82a1a094a0a59931b6df3c96beab35
prasana shinde <prasana.shinde.rebase@gmail.com>
2022-02-21 12:02:39 +0530

    changed auth0 configs for dev environment

--- src/config.development.ts
+++ src/config.development.ts
@@ -39,9 +39,9 @@ export class DevServer {
       if (conn) {
         const { connection } = conn;
         const configConstants = {
-          AUTH_AUDIENCE: 'http://localhost:4000',
-          AUTH_DOMAIN: 'https://dev-bnfcgcth.us.auth0.com/',
-          AUTH_CLIENT_ID: 'B7hdgDYvx7fyGktJJxxidg9qg0Xvbq0s',
+          AUTH_AUDIENCE: 'concertcuration.dev.api',
+          AUTH_DOMAIN: 'https://dev-yga6eln3.us.auth0.com/',
+          AUTH_CLIENT_ID: 'oVngjMIrVInLZTxwFv9k64FS4cQ34d5V',
           AUTH_CONNECTION: 'Username-Password-Authentication',
           AUTH_TOKEN: '',
         };
