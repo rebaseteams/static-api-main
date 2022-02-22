4054b5bf374047fac8f3ad4e95348aeb714fd8eb
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-02-22 10:51:09 +0530

    getActions Route service and PG Repo

--- src/repositories/actions/postgres/actions.ts
+++ src/repositories/actions/postgres/actions.ts
@@ -1,27 +1,29 @@
-import { BaseTypeORMRepo } from '@rebaseitlabs/typeorm-generic';
-import { Connection } from 'typeorm';
+// import { BaseTypeORMRepo } from '@rebaseitlabs/typeorm-generic';
+import { Connection, Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
 import { PgActionEntity } from '../../../models/entities/pg-actions';
 import { ActionInterface } from '../../../models/interfaces/actions';
 import { Action } from '../../../models/types/role';
 
-function mapEntityToType(entity: PgActionEntity): Action {
-  return {
-    id: entity.id,
-    name: entity.name,
-  };
-}
+// function mapEntityToType(entity: PgActionEntity): Action {
+//   return {
+//     id: entity.id,
+//     name: entity.name,
+//   };
+// }
 
-function mapTypeToEntity(type: Action): PgActionEntity {
-  return {
-    id: type.id,
-    name: type.name,
-  };
-}
-export default class ActionsRepo extends BaseTypeORMRepo<Action, PgActionEntity> implements ActionInterface {
-  constructor(connection: Connection, resourceName: string) {
-    super(connection, resourceName, mapEntityToType, mapTypeToEntity);
+// function mapTypeToEntity(type: Action): PgActionEntity {
+//   return {
+//     id: type.id,
+//     name: type.name,
+//   };
+// }
+export default class ActionsRepo implements ActionInterface {
+  private actionsRepo : Repository<PgActionEntity>;
+
+  constructor(connection: Connection) {
+    this.actionsRepo = connection.getRepository(PgActionEntity);
   }
 
   async createAction(name: string): Promise<{action: Action}> {
@@ -29,10 +31,12 @@ export default class ActionsRepo extends BaseTypeORMRepo<Action, PgActionEntity>
       id: uuidv4(),
       name,
     };
-    const resp = await super.create(action);
-    if (resp instanceof PgActionEntity) {
-      return { action: resp };
-    }
-    throw resp;
+    const resp = this.actionsRepo.create(action);
+    return { action: resp };
+  }
+
+  async getActions(): Promise<{ actions: Action[] }> {
+    const resp = await this.actionsRepo.find();
+    return { actions: resp };
   }
 }
