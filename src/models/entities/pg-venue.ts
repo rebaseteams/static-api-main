98deb28e439e2bbbf28d0234a214285d3f36fa32
Rebase Dev <rebase.teams@gmail.com>
2022-02-25 18:03:01 +0530

    sc-1098-Add-venue-columns

--- src/models/entities/pg-venue.ts
+++ src/models/entities/pg-venue.ts
@@ -1,3 +1,4 @@
+import { float } from 'aws-sdk/clients/lightsail';
 import { Entity, Column, PrimaryColumn } from 'typeorm';
 import { Address } from '../types/address';
 
@@ -14,4 +15,40 @@ export default class PgVenueEntity {
 
   @Column()
   capacity: number;
+
+  @Column({ type: 'varchar', default: '' })
+  bowie_venue_id: string;
+
+  @Column({ type: 'varchar', default: '' })
+  url: string;
+
+  @Column({ type: 'float', default: 0.0 })
+  lat: float;
+
+  @Column({ type: 'float', default: 0.0 })
+  long: float;
+
+  @Column({ type: 'varchar', default: '' })
+  street: string;
+
+  @Column({ type: 'varchar', default: '' })
+  city: string;
+
+  @Column({ type: 'varchar', default: '' })
+  state: string;
+
+  @Column({ type: 'varchar', default: '' })
+  postal_code: string;
+
+  @Column({ type: 'varchar', default: '' })
+  country: string;
+
+  @Column({ type: 'text', default: '' })
+  comments?: string;
+
+  @Column({ type: 'varchar', default: '' })
+  last_modified_by?: string;
+
+  @Column({ type: 'date', default: () => 'CURRENT_TIMESTAMP' })
+  last_modified_at?: string;
 }
