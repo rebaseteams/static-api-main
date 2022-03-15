802688cca93a618e2508f3589fb9bcbe08ce7ee7
saritChauhan <saritsinghchauhan@gmail.com>
2022-03-15 16:20:04 +0530

    to get search result from all desired repos

--- src/repositories/advancedSearch/postgres/advancedSearch.ts
+++ src/repositories/advancedSearch/postgres/advancedSearch.ts
@@ -1,27 +1,48 @@
+/* eslint-disable no-return-await */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
+import _ from 'lodash';
 import { Connection, Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
+import { PgArtistEntity } from '../../../models/entities/pg-artist';
+import PgBrandEntity from '../../../models/entities/pg-brand';
+import PgVenueEntity from '../../../models/entities/pg-venue';
 import { AdvancedSearchInterface } from '../../../models/interfaces/adancedSearch';
 import { AdvancedSearchQuery, AdvancedSearchResults } from '../../../models/types/adancedSearch';
 
 export default class AdvacedSearchRepo implements AdvancedSearchInterface {
-    private Conn: Connection
-    // will make use of this connection to find repo
+    private searchRepos: Array<{
+      name: string;
+      repo: Repository<any>
+    }>;
 
     constructor(connection: Connection) {
-      this.Conn = connection;
+      this.searchRepos = [
+        {
+          name: 'artist',
+          repo: connection.getRepository(PgArtistEntity),
+        },
+        {
+          name: 'brand',
+          repo: connection.getRepository(PgBrandEntity),
+        },
+        {
+          name: 'venue',
+          repo: connection.getRepository(PgVenueEntity),
+        },
+      ];
     }
 
     async get(query: AdvancedSearchQuery): Promise<{ results: AdvancedSearchResults; }> {
-      const dummyData = [{
-        id: uuidv4(),
-        image: 'string',
-        title: 'string',
-        description: 'string',
-        type: 'string',
-        destinationUrl: 'string',
-      }];
-      return { results: dummyData };
+      const repoResponseData: any = await Promise.all(
+
+        this.searchRepos.map(async (e) => {
+          const arr = await e.repo.find();
+          return arr;
+        }),
+      );
+
+      const searchData: AdvancedSearchResults = _.flatten(repoResponseData);
+      return { results: searchData };
     }
 }
