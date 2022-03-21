5272aa877565521a3d1c30017c14b3dc23889907
saritChauhan <saritsinghchauhan@gmail.com>
2022-03-21 15:23:11 +0530

    search by query from selected resources

--- src/utils/desiredSearchRepos.ts
+++ src/utils/desiredSearchRepos.ts
@@ -6,14 +6,28 @@ import PgVenueEntity from '../models/entities/pg-venue';
 const getDesiredSearchRepos = (connection: Connection) => [{
   name: 'artist',
   repo: connection.getRepository(PgArtistEntity),
+  options: {
+    select: ['id', 'name', 'country'],
+    searchFrom: ['name', 'country'],
+  },
 },
 {
   name: 'brand',
   repo: connection.getRepository(PgBrandEntity),
+  options: {
+    select: ['id', 'name'],
+    searchFrom: ['name'],
+
+  },
 },
 {
   name: 'venue',
   repo: connection.getRepository(PgVenueEntity),
+  options: {
+    select: ['id', 'name'],
+    searchFrom: ['name'],
+
+  },
 },
 ];
 
