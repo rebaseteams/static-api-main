76e17ac836ea0543b0eb1729d150cab1cda7bdf1
saritChauhan <saritsinghchauhan@gmail.com>
2022-03-21 11:58:09 +0530

    added invert if conditions

--- src/providers/advancedSearchProvider.ts
+++ src/providers/advancedSearchProvider.ts
@@ -3,33 +3,30 @@ import { AdvancedSearchInterface } from '../models/interfaces/adancedSearch';
 import { AdvancedSearchDesiredRepos, AdvancedSearchQuery, AdvancedSearchResults } from '../models/types/adancedSearch';
 
 export default class AdvancedSearchProvider implements AdvancedSearchInterface {
-private desiredRepos: AdvancedSearchDesiredRepos;
+    private desiredRepos: AdvancedSearchDesiredRepos;
 
-constructor(desiredRepos: AdvancedSearchDesiredRepos) {
-  this.desiredRepos = desiredRepos;
-}
-
-async get(query: AdvancedSearchQuery): Promise<{ results: AdvancedSearchResults; }> {
-  let filteredRepos: AdvancedSearchDesiredRepos;
-  if (query.category) {
-    const validCategory = !!this.desiredRepos.find((i) => i.name === query.category);
-    if (validCategory) {
-      filteredRepos = this.desiredRepos.filter((r) => r.name === query.category);
-    } else {
-      filteredRepos = this.desiredRepos;
+    private filterDesiredRepos(category: string) {
+      if (!category) return this.desiredRepos;
+      const validCategory = !!this.desiredRepos.find((i) => i.name === category);
+      if (!validCategory) return this.desiredRepos;
+      return this.desiredRepos.filter((r) => r.name === category);
     }
-  } else {
-    filteredRepos = this.desiredRepos;
-  }
-  const repoResponseData: any = await Promise.all(
 
-    filteredRepos.map(async (e) => {
-      const arr = await e.repo.find();
-      return arr;
-    }),
-  );
+    constructor(desiredRepos: AdvancedSearchDesiredRepos) {
+      this.desiredRepos = desiredRepos;
+    }
 
-  const searchData: AdvancedSearchResults = _.flatten(repoResponseData);
-  return { results: searchData };
-}
+    async get(query: AdvancedSearchQuery): Promise<{ results: AdvancedSearchResults; }> {
+      const filteredRepos = this.filterDesiredRepos(query.category);
+
+      const repoResponseData: any = await Promise.all(
+        filteredRepos.map(async (e) => {
+          const arr = await e.repo.find();
+          return arr;
+        }),
+      );
+
+      const searchData: AdvancedSearchResults = _.flatten(repoResponseData);
+      return { results: searchData };
+    }
 }
