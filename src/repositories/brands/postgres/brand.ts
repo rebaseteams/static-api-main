0d5e44111f53610277a07a7fc7be365e5f998bbf
saritChauhan <saritsinghchauhan@gmail.com>
2022-05-16 12:08:14 +0530

    used mock addOnData in brand

--- src/repositories/brands/postgres/brand.ts
+++ src/repositories/brands/postgres/brand.ts
@@ -2,8 +2,14 @@
 import { Connection, Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 import PgBrandEntity from '../../../models/entities/pg-brand';
-import { Brand, demographics as Demo, media_handles as Media } from '../../../models/types/brand';
+import {
+  Brand,
+  demographics as Demo, media_handles as Media, ShortBrand,
+} from '../../../models/types/brand';
 import { BrandsInterface } from '../../../models/interfaces/brand';
+import brandAffinity from '../../artists/postgres/addOnData/brandAffinity';
+import { youtubeinsights } from '../../artists/postgres/addOnData/youtubeInsights';
+import latestReleaseVideos from '../../artists/postgres/addOnData/latestReleaseVideos';
 
 export default class BrandRepo implements BrandsInterface {
     private brandRepository : Repository<PgBrandEntity>;
@@ -12,7 +18,14 @@ export default class BrandRepo implements BrandsInterface {
       this.brandRepository = connection.getRepository(PgBrandEntity);
     }
 
-    async createBrand(name : string, logo : string, website : string, contact : string, bowie_brand_id: string, demographics: Demo, media_handles: Media, industry: string[], comments?: string, userId?: string) : Promise<{brand : Brand}> {
+    async createBrand(name: string,
+      logo: string,
+      website: string,
+      contact: string,
+      bowie_brand_id: string, demographics: Demo,
+      media_handles: Media, industry: string[],
+      comments?: string,
+      userId?: string): Promise<{ brand: ShortBrand }> {
       const brand: PgBrandEntity = {
         id: uuidv4(),
         name,
@@ -33,7 +46,9 @@ export default class BrandRepo implements BrandsInterface {
 
     async getBrand(id : string) : Promise<Brand> {
       const brand = await this.brandRepository.findOne({ id });
-      if (brand) return brand;
+      if (brand) {
+        return this.convertToTypeBrand(brand);
+      }
       const err = { message: `Brand not found for id: ${id}`, statusCode: 404 };
       throw err;
     }
@@ -60,11 +75,12 @@ export default class BrandRepo implements BrandsInterface {
     }
 
     async getBrands(skip : number, limit : number) : Promise<Brand[]> {
-      const brands : Brand[] = await this.brandRepository.find({
+      const brands: PgBrandEntity[] = await this.brandRepository.find({
         take: limit,
         skip,
       });
-      return brands;
+      const newBrand = brands.map((b) => this.convertToTypeBrand(b));
+      return newBrand;
     }
 
     async getAllBrands() : Promise<Array<{id: string, name: string}>> {
@@ -72,5 +88,25 @@ export default class BrandRepo implements BrandsInterface {
         select: ['id', 'name'],
       });
       return brands;
+    }
+
+    convertToTypeBrand(brand: PgBrandEntity): Brand {
+      const toSendBrand: Brand = {
+        id: brand.id,
+        name: brand.name,
+        logo: brand.logo,
+        website: brand.website,
+        contact: brand.contact,
+        bowie_brand_id: brand.bowie_brand_id,
+        demographics: brand.demographics,
+        // TO-DO: LatestYoutubeReleases,BrandAffinity,YoutubeInsights table
+        media_handles: brand.media_handles,
+        brand_affinity: brandAffinity,
+        latest_youtube_release: latestReleaseVideos,
+        youtube_insights: youtubeinsights,
+        industry: brand.industry,
+        last_modified_at: brand.last_modified_at,
+      };
+      return toSendBrand;
     }
 }
