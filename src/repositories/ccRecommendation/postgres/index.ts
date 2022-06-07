17b0f4533dea3d50acb132401215ca60af6977db
Virajj <viraj.jadhav.rebase@gmail.com>
2022-06-07 11:01:19 +0530

    message published in recommendation queue

--- src/repositories/ccRecommendation/postgres/index.ts
+++ src/repositories/ccRecommendation/postgres/index.ts
@@ -1,21 +1,35 @@
 /* eslint-disable no-console */
-import axios from 'axios';
+import * as AWS from 'aws-sdk';
 import { CCRecommendationInterface } from '../../../models/interfaces/ccRecommendation';
 
 export default class CCRecommendationRepo implements CCRecommendationInterface {
-    Uri: string;
+    sns: AWS.SNS;
 
-    constructor(cc_recomm_server: string) {
-      this.Uri = `${cc_recomm_server}/artist-recommendation`;
+    constructor() {
+      AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+        region: process.env.aws_region,
+      });
+      this.sns = new AWS.SNS({ apiVersion: '2010-03-31' });
     }
 
-    generateRecommendation = async (id: string): Promise<{ success: boolean; }> => new Promise((resolve, reject) => {
-      axios.post(this.Uri, { id }).then(() => {
-        resolve({
-          success: true,
-        });
-      }).catch((err) => {
-        reject(err);
-      });
+    generateRecommendation = async (id: string): Promise<{ success: boolean; }> => new Promise(() => {
+      const params = {
+        Message: JSON.stringify({ id }),
+        TopicArn: process.env.TopicArn_SNS_Generate_Recommendation,
+      };
+      const publishTextPromise = this.sns.publish(params).promise();
+
+      publishTextPromise.then(
+        (data) => {
+          console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+          console.log(`MessageID is ${data.MessageId}`);
+        },
+      ).catch(
+        (err) => {
+          console.error(err, err.stack);
+        },
+      );
     })
 }
